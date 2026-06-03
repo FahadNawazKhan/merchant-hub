3f8351083f5d78f345ee6bc2c40590d7e59d28d8
Fahad Nawaz <faddukhan3@gmail.com>
2026-06-03 15:16:46 +0000

    auth contorllers and routers

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,37 @@
+import { User } from "../models/Users.js";
+import { hashPassword } from "../utils/hashPassword.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({
+      email,
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await hashPassword(
+      password
+    );
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
