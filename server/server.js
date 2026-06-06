2ee1acfc722c4ce4b8ec59ef535332ff4bcdf768
Fahad Nawaz <faddukhan3@gmail.com>
2026-06-06 17:47:08 +0000

    route handler

--- server/server.js
+++ server/server.js
@@ -15,10 +15,13 @@ server.use(cors())
 connectDB();
 
 
-server.use('/api/auth',authRouter);
-server.use('/api/products',productRouter);
-server.use('/api/cart',cartRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/products', productRouter);
+server.use('/api/cart', cartRouter);
 
+server.use((req, res) => {
+  res.send('route not found')
+})
 
 server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
